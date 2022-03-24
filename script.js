96cb01c206638c77ae0ae278181111583acc1947
sf-002 <sf-002@ya.ru>
2022-03-24 19:00:47 +0300

    Semi-Optional

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-//USD => CNY
-//https://www.codewars.com/kata/5977618080ef220766000022
-function usdcny(usd) {
-    return (usd*6.75).toFixed(2)+' Chinese Yuan';
+//Semi-Optional
+//https://www.codewars.com/kata/521cd52e790405a74800032c
+function wrap(value) {
+    return  {
+      value: value 
+    };
   }

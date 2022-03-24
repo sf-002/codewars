dffcb8ecf3e6b53f56a72ed0f735bb630dd5f45c
sf-002 <sf-002@ya.ru>
2022-03-24 20:18:37 +0300

    Pascal's Diagonals

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-//Will you make it?
-//https://www.codewars.com/kata/5861d28f124b35723e00005e
-const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
-    return (distanceToPump / mpg <= fuelLeft);
-  };
+//Pascal's Diagonals
+//https://www.codewars.com/kata/576b072359b1161a7b000a17
+function factorial(n) {
+    return (n > 1) ? n * factorial(n - 1) : 1;
+  }
+  function pascTriElement (n, m) {
+    return factorial(n) / (factorial(m) * factorial(n - m));
+  }
+  function generateDiagonal(n, l) {
+    let diagonal = [];
+    for (let i = 0; i < l; i++){
+      diagonal.push( Math.round(pascTriElement(n, i)) );
+      n++;
+    }
+    return diagonal;
+    // return an array containing the numbers in the nth diagonal of Pascal's triangle, to the specified length
+  }

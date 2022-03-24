7bc41108327306bbba6b507e3549db3b9c9e62a5
sf-002 <sf-002@ya.ru>
2022-03-24 18:57:47 +0300

    USD => CNY

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//Pillars
-//https://www.codewars.com/kata/5bb0c58f484fcd170700063d
-function pillars(num_pill, dist, width) {
-    return Math.max(0, dist * (num_pill - 1) * 100 + width * (num_pill - 2));
+//USD => CNY
+//https://www.codewars.com/kata/5977618080ef220766000022
+function usdcny(usd) {
+    return (usd*6.75).toFixed(2)+' Chinese Yuan';
   }

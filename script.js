c3a64e7c410d1b1a3edeaae721c7c8661aa2fe8a
sf-002 <sf-002@ya.ru>
2022-03-25 17:40:59 +0300

    Assemble string

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
-//bread sandwiches
-//https://www.codewars.com/kata/622a6a822494ab004b2c68d2
-function slicesToName(n) {
-  let phrase = '';
-
-  if (n > 3) {
-    if (n % 2 == 1) {
-      phrase = 'bread sandwich';// + (' sandwich'*((n-3)/2))
-      for (let i = 0; i < (n-3)/2; i++) phrase += ' sandwich';
+//Assemble string
+//https://www.codewars.com/kata/6210fb7aabf047000f3a3ad6
+function assembleString(array){
+  if (typeof array == 'undefined') array = [];
+  if (typeof array[0] == 'undefined') array[0] = 0;
+//  const wordLen = array[0].length;
+//  const arrLen = array.length;
+  let word = [];
+  for (let i = 0; i < array[0].length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j][i] == '*') word[i] = '#'
+      else {
+          word[i] = array[j][i];
+          break;
+        }
       }
-    else {
-      phrase = 'sandwich';// + ' sandwich'*(n/2-1);
-      for (let i = 0; i < n/2-1; i++) phrase += ' sandwich';
-      }
-  } else if (n > 1){
-    if (n % 2 == 1) phrase = 'bread sandwich'
-    else phrase = 'sandwich';
-  } else {
-    phrase = null;
-  }
-  return phrase;
+    }
+  return word.join('');
 }
-
-function nameToSlices(name) {
-  let numOfBreads = 0;
-  let cookArr;
-  if (typeof name == 'string') cookArr = name.split(' ')
-  else return null;
-  for (let i = cookArr.length - 1; i >= 0; i--) {
-    if (cookArr[i] == 'bread' && i == 0 && numOfBreads != 0) numOfBreads++
-    else if (cookArr[i] == 'sandwich') numOfBreads += 2
-    else //if (cookArr[i] != 'sandwich'){
-      return null;  
-    //  }
-  }
-  return numOfBreads;
-}
-
-// have fun!

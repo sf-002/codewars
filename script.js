3555d0f97c17142d4ce95b422a6577ae005283a4
sf-002 <sf-002@ya.ru>
2022-03-25 17:38:27 +0300

    bread sandwiches

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-//Pascal's Diagonals
-//https://www.codewars.com/kata/576b072359b1161a7b000a17
-function factorial(n) {
-    return (n > 1) ? n * factorial(n - 1) : 1;
+//bread sandwiches
+//https://www.codewars.com/kata/622a6a822494ab004b2c68d2
+function slicesToName(n) {
+  let phrase = '';
+
+  if (n > 3) {
+    if (n % 2 == 1) {
+      phrase = 'bread sandwich';// + (' sandwich'*((n-3)/2))
+      for (let i = 0; i < (n-3)/2; i++) phrase += ' sandwich';
+      }
+    else {
+      phrase = 'sandwich';// + ' sandwich'*(n/2-1);
+      for (let i = 0; i < n/2-1; i++) phrase += ' sandwich';
+      }
+  } else if (n > 1){
+    if (n % 2 == 1) phrase = 'bread sandwich'
+    else phrase = 'sandwich';
+  } else {
+    phrase = null;
   }
-  function pascTriElement (n, m) {
-    return factorial(n) / (factorial(m) * factorial(n - m));
+  return phrase;
+}
+
+function nameToSlices(name) {
+  let numOfBreads = 0;
+  let cookArr;
+  if (typeof name == 'string') cookArr = name.split(' ')
+  else return null;
+  for (let i = cookArr.length - 1; i >= 0; i--) {
+    if (cookArr[i] == 'bread' && i == 0 && numOfBreads != 0) numOfBreads++
+    else if (cookArr[i] == 'sandwich') numOfBreads += 2
+    else //if (cookArr[i] != 'sandwich'){
+      return null;  
+    //  }
   }
-  function generateDiagonal(n, l) {
-    let diagonal = [];
-    for (let i = 0; i < l; i++){
-      diagonal.push( Math.round(pascTriElement(n, i)) );
-      n++;
-    }
-    return diagonal;
-    // return an array containing the numbers in the nth diagonal of Pascal's triangle, to the specified length
-  }
+  return numOfBreads;
+}
+
+// have fun!

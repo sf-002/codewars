e7dc8fb20ecb013213536983581e9d3b8b652dec
sf-002 <sf-002@ya.ru>
2022-03-25 19:32:57 +0300

    Find the odd int

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-//Multiples of 3 or 5
-//https://www.codewars.com/kata/514b92a657cdc65150000006
-function solution(number){
-  var sum = 0;
-  
-  for(var i = 1;i< number; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      sum += i
-    }
+//Find the odd int
+//https://www.codewars.com/kata/54da5a58ea159efa38000836
+function findOdd(A) {
+  let oddNum = 0;
+  //A.forEach(function (item, index, arr) {
+  let objOfAppears = A.reduce( function(accArr, item) {
+      accArr[item] = (accArr[item] || 0) + 1;
+      return accArr;
+      }, {});
+  for (let key in objOfAppears) {
+    if (objOfAppears[key] % 2 == 1) {
+      oddNum = parseInt(key);
+      break;
+      }
   }
-  return sum;
+  return oddNum;
 }

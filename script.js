b3834ea56bbb5e948b9fb5430ccb74a48653b60d
sf-002 <sf-002@ya.ru>
2022-03-26 19:58:06 +0300

    Take a Ten Minutes Walk

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-//Find The Parity Outlier
-//https://www.codewars.com/kata/5526fc09a1bbd946250002dc
-function findOutlier(integers){
-  let j = 0;
-  for (let i = 0; i < 3; i++) {
-    j += Math.abs(integers[i] % 2);
-  };
-  let remainder = 0;
-  if (j <= 1) remainder = 1;
-  integers = new Set(integers);
-  let i;
-  for (let value of integers) if (Math.abs(value % 2) == remainder) return value;
+//Take a Ten Minutes Walk
+//https://www.codewars.com/kata/54da539698b8a2ad76000228
+function isValidWalk(walk) {
+  if (walk.length == 10) {
+    if (walk.reduce(function(value, item) {
+      if (item == 's') value += 1
+      else if (item == 'n') value -= 1;
+      return value;
+    }, 0) == 0 
+       &&
+       walk.reduce(function(value, item) {
+      if (item == 'w') value += 1
+      else if (item == 'e') value -= 1;
+      return value;
+    }, 0) == 0  ) return true;
+  } else return false; 
 }
+//самое унылое нечитаемое решение

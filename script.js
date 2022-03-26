58f6788d5c8d9b6b7e0907a2a38241f1794e5b7d
sf-002 <sf-002@ya.ru>
2022-03-26 21:27:14 +0300

    Highest and Lowest

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 //Disemvowel Trolls
-//https://www.codewars.com/kata/52fba66badcd10859f00097e
-function disemvowel(str) {
-  const vowels = new Set('aeiouAEIOU');
-  let i = 0, 
-      newStr = '';
-  for (let char of str) {
-    if (!vowels.has(char)) newStr += str[i];
-    i++;
-  }  
-  return newStr;
+//https://www.codewars.com/kata/554b4ac871d6813a03000035
+function highAndLow(numbers){
+  let arr = numbers.split(' ');
+  arr.sort( (a, b) => a - b );
+  return `${arr[arr.length - 1]} ${arr[0]}`
 }

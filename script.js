1de5f828f2f11818b12f6aa59eb4c017e7bb9767
sf-002 <sf-002@ya.ru>
2022-03-26 21:03:46 +0300

    Vowel Count

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-//Persistent Bugger.
-//https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec
-function multiply(num) {
-  let word = num.toString();
-  num = 1;
-  for (let numChar of word) {
-    num *= Number(numChar);
-  };
-  return num;
-}
-
-function persistence(num) {
-  let i = 0;
-  while (num > 9) {
-    num = multiply(num);
-    i++;
-  };
-  return i;
+//Vowel Count
+//https://www.codewars.com/kata/54ff3102c1bad923760001f3
+function getCount(str) {
+  var vowelsCount = 0;
+  const vowels = new Set('aeiou');
+  for (let char of str) {
+    if (vowels.has(char)) vowelsCount++;
+  }  
+  return vowelsCount;
 }
